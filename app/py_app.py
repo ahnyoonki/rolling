4b257813c553bbc468052c3ec7eddb7ff0e6087c
root <yg1700@naver.com>
2021-08-20 05:41:22 +0900

    17 commit

--- app/py_app.py
+++ app/py_app.py
@@ -7,7 +7,7 @@ def python_flask():
       Docker container application: Python & Flask!</h1>
       <p style="text-algin:center; color:deepskyblue;">
         This is micro web framework for running Flask
-        inside Docker<15>.</p>
+        inside Docker<16>.</p>
     """
 
 if __name__ == '__main__':
