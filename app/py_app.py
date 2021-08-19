8dd94093405dc1771841c94196c370795382fe6d
root <yg1700@naver.com>
2021-08-20 01:18:00 +0900

    first commit

---
+++ app/py_app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+py_app =Flask(__name__)
+@py_app.route('/')
+def python_flask():
+    return """
+    <h1 style="text-algin:center; color:deepskyblue;">
+      Docker container application: Python & Flask!</h1>
+      <p style="text-algin:center; color:deepskyblue;">
+        This is micro web framework for running Flask
+        inside Docker<1>.</p>
+    """
+
+if __name__ == '__main__':
+    py_app.run(host='0.0.0.0', port=9000, debug=True)
+
